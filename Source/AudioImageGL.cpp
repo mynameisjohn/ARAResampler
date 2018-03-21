ed4769058b1d517a4601a36f956e4e2378ac155c
mynameisjohn <mynameisjohnj@gmail.com>
2018-03-21 09:09:47 -0400

    Adding comments to the AudioImageGL class

--- Source/AudioImageGL.cpp
+++ Source/AudioImageGL.cpp
@@ -1,8 +1,10 @@
 #include "AudioImageGL.h"
 #include "PluginEditor.h"
 
+// initialise override 
 void AudioImageGL::initialise()
 {
+	// We only care about position and uniform color
 	vertexShader =
 		"attribute vec2 position;\n"
 		"\n"
@@ -20,6 +22,7 @@ void AudioImageGL::initialise()
 		"    gl_FragColor = color;\n"
 		"}\n";
 
+	// Compile and link shader
 	std::unique_ptr<OpenGLShaderProgram> newShader ( new OpenGLShaderProgram ( openGLContext ) ); // [1]
 	String statusText;
 	if ( newShader->addVertexShader ( OpenGLHelpers::translateVertexShaderToV3 ( vertexShader ) )  // [2]
@@ -38,41 +41,55 @@ void AudioImageGL::initialise()
 	{
 		statusText = newShader->getLastError();                                               // [4]
 	}
-
+	
+	// set default color to red
 	m_Color = Colours::red;
 }
 
+// Public access to drawn waveform
+// If the GL thread sees m_pSampleToDraw is not null
+// it will recrate its geometry to match the sample
 void AudioImageGL::SetWave( UniSampler::Sample * pSample )
 {
 	std::lock_guard<std::mutex> gl( m_muGL );
 	m_pSampleToDraw = pSample;
 }
 
+// Set color of drawn waveform
 void AudioImageGL::SetColor( Colour c )
 {
 	std::lock_guard<std::mutex> gl( m_muGL );
 	m_Color = c;
 }
 
-void AudioImageGL::createWaveFromSample()
+// Create a waveform drawing from an audio sample
+void AudioImageGL::createWaveFromSample( UniSampler::Sample * pSample, int nLines /*= 2000*/ )
 {
-	if ( m_pSampleToDraw == nullptr )
+	if ( pSample == nullptr )
 		return;
 
 	// Create geometry
-	int nPoints = 1000;
 	std::vector<Vertex> vPositionData;
 	if ( m_pSampleToDraw->bStereo )
 	{
+		
+		// 
 		for ( int c = 0; c < 2; c++ )
 		{
-			for ( int i = 0; i < nPoints; i++ )
+			for ( int i = 0; i < nLines; i++ )
 			{
-				float x = dsp::Remap<float>( i, 0, nPoints, -1, 1 );
-				int ixSample = dsp::Remap<double>( i, 0, nPoints, 0, m_pSampleToDraw->size() );
+				// Find x position relative to how many lines we're drawing
+				float x = dsp::Remap<float>( i, 0, nLines, -1, 1 );
+
+				// Sample level is Y pos, use line index to find corresponding sample index
+				int ixSample = dsp::Remap<double>( i, 0, nLines, 0, m_pSampleToDraw->size() );
 				float y = m_pSampleToDraw->at( 0, ixSample );
+
+				// Change short to normalized float if necessary
 				if ( !m_pSampleToDraw->bFloat )
 					y /= SHRT2FLT;
+
+				// For stereo samples draw each channel between [-1, 0] and [0, 1]
 				y = dsp::Remap<float>( y, -1, 1, -1 + c, c );
 				vPositionData.push_back( { x, -0.5f + c } );
 				vPositionData.push_back( { x, y } );
@@ -81,26 +98,32 @@ void AudioImageGL::createWaveFromSample()
 	}
 	else
 	{
-		for ( int i = 0; i < nPoints; i++ )
+		
+		for ( int i = 0; i < nLines; i++ )
 		{
-			float x = dsp::Remap<float>( i, 0, nPoints, -1, 1 );
-			int ixSample = dsp::Remap<double>( i, 0, nPoints, 0, m_pSampleToDraw->size() );
+			// Find x position relative to how many lines we're drawing
+			float x = dsp::Remap<float>( i, 0, nLines, -1, 1 );
+
+			// Sample level is y pos, use line index to find corresponding sample index
+			int ixSample = dsp::Remap<double>( i, 0, nLines, 0, m_pSampleToDraw->size() );
 			float y = m_pSampleToDraw->at( 0, ixSample );
+
+			// Change short to normalized float if necessary
 			if ( !m_pSampleToDraw->bFloat )
 				y /= SHRT2FLT;
+
 			vPositionData.push_back( { x, 0 } );
 			vPositionData.push_back( { x, y } );
 		}
 	}
 
+	// Reconstruct VBO (this will erase the old VBO)
 	vertices.reset( new VertexBuffer( openGLContext, vPositionData ) );
-
-	m_pSampleToDraw = nullptr;
 }
 
+// The waveform area represents our "open sample" button
 void AudioImageGL::mouseDown( const MouseEvent& e )
 {
-	// This is our "open sample" button
 	FileChooser chooser( "Open an Sample File...",
 						 File::nonexistent,
 						 "*.wav;*.ogg;*.flac" );
@@ -112,19 +135,19 @@ void AudioImageGL::mouseDown( const MouseEvent& e )
 	}
 }
 
-AudioImageGL::AudioImageGL( JuceSamplerEditor * pEditor ) :
+AudioImageGL::AudioImageGL( ARAResamplerEditor * pEditor ) :
 	m_pEditor( pEditor )
 	, m_pSampleToDraw( nullptr )
-{
+{}
 
-}
-
+// Shutdown and shutdown openGL
 AudioImageGL::~AudioImageGL()
 {
 	shutdown();
 	shutdownOpenGL();
 }
 
+// Reset any GL members
 void AudioImageGL::shutdown()
 {
 	// Free any GL objects created for rendering here.
@@ -133,24 +156,40 @@ void AudioImageGL::shutdown()
 	uniforms.reset();
 }
 
+// Update the OpenGL data
 void AudioImageGL::render()
 {
-	std::lock_guard<std::mutex> lg( m_muGL );
-
-	if ( m_pSampleToDraw )
+	// Potentially updated variables
+	Colour colorToUse;
+	UniSampler::Sample * pSampleToDraw = nullptr;
 	{
-		createWaveFromSample();
+		// Keep this locked while we look for a new sample or color
+		std::lock_guard<std::mutex> lg( m_muGL );
+
+		// Cache these while mutex is locked
+		// Always null out m_pSampleToDraw so we 
+		// don't redraw it unnecessarily
+		colorToUse = m_Color;
+		pSampleToDraw = m_pSampleToDraw;
+		m_pSampleToDraw = nullptr;
 	}
 
+	// Maybe redraw sample
+	if ( pSampleToDraw )
+	{
+		createWaveFromSample( pSampleToDraw );
+	}
+
+	// If we have no vertex data then get out
 	if ( vertices == nullptr )
 		return;
 
 	// This clears the context with a black background.
 	OpenGLHelpers::clear ( Colours::black );
 
-	// Add your rendering code here...    
+	// Bind shader
 	jassert ( OpenGLHelpers::isContextActive() );
-	auto desktopScale = (float) openGLContext.getRenderingScale(); // [1]
+	float desktopScale = (float) openGLContext.getRenderingScale(); // [1]
 	OpenGLHelpers::clear ( getLookAndFeel().findColour ( ResizableWindow::backgroundColourId ) ); // [2]
 	glEnable ( GL_BLEND );                                           // [3]
 	glBlendFunc ( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
@@ -160,20 +199,24 @@ void AudioImageGL::render()
 	// Set color
 	uniforms->color->set( m_Color.getFloatRed(), m_Color.getFloatGreen(), m_Color.getFloatBlue(), m_Color.getFloatAlpha() );
 
+	// Draw geometry
 	vertices->bind();
 	attributes->enable( openGLContext );
 	glDrawElements( GL_LINES, vertices->nIndices, GL_UNSIGNED_INT, 0 );
 	attributes->disable( openGLContext );
 
+	// Clear bound buffers
 	openGLContext.extensions.glBindBuffer ( GL_ARRAY_BUFFER, 0 );    // [9]
 	openGLContext.extensions.glBindBuffer ( GL_ELEMENT_ARRAY_BUFFER, 0 );
 }
 
+// This must be implemented, but we don't really need it
 void AudioImageGL::paint( Graphics& g )
 {
 
 }
 
+// Our attributes constructor simply creates an attribute called position, which should exist in the shader
 AudioImageGL::Attributes::Attributes( OpenGLContext& openGLContext, OpenGLShaderProgram& shaderProgram )
 {
 	String strAttrName = "position";
@@ -182,6 +225,8 @@ AudioImageGL::Attributes::Attributes( OpenGLContext& openGLContext, OpenGLShader
 		position.reset( new OpenGLShaderProgram::Attribute( shaderProgram, strAttrName.toRawUTF8() ) );
 }
 
+// Configure the position attribute as a 2D float
+// (actual array data is uploaded in the VertexBuffer constructor)
 void AudioImageGL::Attributes::enable( OpenGLContext& openGLContext )
 {
 	if ( position )
@@ -191,6 +236,7 @@ void AudioImageGL::Attributes::enable( OpenGLContext& openGLContext )
 	}
 }
 
+// Disable position array 
 void AudioImageGL::Attributes::disable( OpenGLContext& openGLContext )
 {
 	if ( position )
@@ -199,6 +245,7 @@ void AudioImageGL::Attributes::disable( OpenGLContext& openGLContext )
 	}
 }
 
+// Our uniform constructor finds the color uniform variable in the shader
 AudioImageGL::Uniforms::Uniforms( OpenGLContext& openGLContext, OpenGLShaderProgram& shaderProgram )
 {
 	String strUniName = "color";
@@ -207,32 +254,39 @@ AudioImageGL::Uniforms::Uniforms( OpenGLContext& openGLContext, OpenGLShaderProg
 		color.reset( new OpenGLShaderProgram::Uniform( shaderProgram, strUniName.toRawUTF8() ) );
 }
 
+// VertexBuffer constructor creates position and index data from a collection of 2D floats
 AudioImageGL::VertexBuffer::VertexBuffer( OpenGLContext& context, const std::vector<Vertex>&vPositionData ) :
 	openGLContext( context )
 	, VBO( 0 )
 	, IBO( 0 )
 	, nIndices( 0 )
 {
+	// Create and upload position data
 	openGLContext.extensions.glGenBuffers( 1, &VBO );
 	openGLContext.extensions.glBindBuffer( GL_ARRAY_BUFFER, VBO );
 	openGLContext.extensions.glBufferData( GL_ARRAY_BUFFER, vPositionData.size() * sizeof( Vertex ), vPositionData.data(), GL_STATIC_DRAW );
 
+	// Find indices - we're drawing lines so this is just 
+	// a series of ordered pairs from beginning to end
 	nIndices = vPositionData.size();
 	std::vector<GLuint> vIndices( nIndices );
 	for ( GLuint i = 0; i < nIndices; i++ )
 		vIndices[i] = i;
 
+	// Upload index data
 	openGLContext.extensions.glGenBuffers( 1, &IBO );
 	openGLContext.extensions.glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, IBO );
 	openGLContext.extensions.glBufferData( GL_ELEMENT_ARRAY_BUFFER, nIndices * sizeof( GLuint ), vIndices.data(), GL_STATIC_DRAW );
 }
 
+// Delete our position and index data
 AudioImageGL::VertexBuffer::~VertexBuffer()
 {
 	openGLContext.extensions.glDeleteBuffers ( 1, &VBO );
 	openGLContext.extensions.glDeleteBuffers ( 1, &IBO );
 }
 
+// Assign vertex position and index to their corresponding GL locations
 void AudioImageGL::VertexBuffer::bind()
 {
 	openGLContext.extensions.glBindBuffer ( GL_ARRAY_BUFFER, VBO );
--- Source/AudioImageGL.h
+++ Source/AudioImageGL.h
@@ -1,9 +1,20 @@
+/***************************************************************
+	AudioImageGL class
+
+A JUCE OpenGL component that we use to render a sample
+waveform using OpenGL lines. A lot of this design was
+taken from the JUCE OpenGL example 
+https://docs.juce.com/master/tutorial_open_gl_application.html
+
+***************************************************************/
+
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "../UniSampler/Source/Plugin/UniSampler.h"
 
-class JuceSamplerEditor;
+// Forward declare our editor class
+class ARAResamplerEditor;
 
 class AudioImageGL : public OpenGLAppComponent
 {
@@ -15,11 +26,13 @@ class AudioImageGL : public OpenGLAppComponent
 
 	void mouseDown( const MouseEvent& e ) override;
 
+	// 2D Vertex struct
 	struct Vertex
 	{
 		float position[2];
 	};
 
+	// Our per-vertex attributes - all we care about is position
 	struct Attributes
 	{
 		//...
@@ -28,12 +41,16 @@ class AudioImageGL : public OpenGLAppComponent
 		void enable( OpenGLContext& openGLContext );
 		void disable( OpenGLContext& openGLContext );
 	};
+
+	// Uniforms - the only one we use is color
 	struct Uniforms
 	{
 		//...
 		std::unique_ptr<OpenGLShaderProgram::Uniform> color;
 		Uniforms ( OpenGLContext& openGLContext, OpenGLShaderProgram& shaderProgram );
 	};
+
+	// Our VBO object - just position and index buffers are needed
 	struct VertexBuffer
 	{
 		GLuint VBO, IBO;
@@ -45,24 +62,39 @@ class AudioImageGL : public OpenGLAppComponent
 	};
 
 	//==============================================================================
-	// Your private member variables go here...
+	// Shader program and variables
 	String vertexShader;
 	String fragmentShader;
 	std::unique_ptr<OpenGLShaderProgram> shader;
 	std::unique_ptr<Attributes> attributes;
 	std::unique_ptr<Uniforms> uniforms;
+
+	// VBO
 	std::unique_ptr<VertexBuffer> vertices;
 	
-	std::mutex m_muGL;
+	// The color we use to render
 	juce::Colour m_Color;
+
+	// Our drawn sample - if this is not null 
+	// then we refresh the image and null it
 	UniSampler::Sample * m_pSampleToDraw;
 
-	JuceSamplerEditor * m_pEditor;
-	void createWaveFromSample();
+	// Protects access to the OpenGL thread
+	std::mutex m_muGL;
+
+	// Editor pointer
+	ARAResamplerEditor * m_pEditor;
+
+	// Function to create wave geometry from sample
+	// The number of lines controls the resolution of the wave
+	void createWaveFromSample( UniSampler::Sample * pSample, int nPoints = 2000 );
 
 public:
-	AudioImageGL( JuceSamplerEditor * pEditor );
+	AudioImageGL( ARAResamplerEditor * pEditor );
 	~AudioImageGL();
+
+	// Public access to wave and color
+	// These lock the GL mutex
 	void SetWave( UniSampler::Sample * pSample );
 	void SetColor( Colour c );
 };
